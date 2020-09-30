7406a95f86be6305c0a7b7453ecdfe714829cdca
AmorFatiBA <you@example.com>
2020-10-01 01:45:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Npgsql;
+
+namespace Version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = "Host=localhost;Username=postgres;Password=Amorfati2020.;Database=dburun";
+
+            using var coni = new NpgsqlConnection(cs);
+            coni.Open();
+
+            var sql = "select * from musteri where bakiye>2000 order by id";
+
+            using var cmd = new NpgsqlCommand(sql, coni);
+
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"PostgreSQL version: {Int32.Parse(reader["id"].ToString())} {reader["ad"].ToString()} {reader["soyad"].ToString()} {reader["sehir"].ToString()} {Int32.Parse(reader["bakiye"].ToString())}");
+                
+                // val = Int32.Parse(reader[0].ToString());
+                //do whatever you like
+            }
+            // var version = cmd.ExecuteNonQuery();
+            // Console.WriteLine($"PostgreSQL version: {version}");
+        }
+    }
+}
